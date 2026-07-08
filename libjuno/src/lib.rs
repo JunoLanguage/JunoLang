2545211d1a5d6f68a3f094ac8e188fbc44f55432
trollmii <trollmii@proton.me>
2026-07-08 16:32:36 +0200

    v0.1.8

--- libjuno/src/lib.rs
+++ libjuno/src/lib.rs
@@ -1,19 +1,43 @@
 
-
+#[cfg(feature = "grammar")]
 mod grammar;
-mod parser;
-pub mod ast;
-mod metair;
-mod ir;
-mod builtin_registry;
-mod compile;
+#[cfg(feature = "grammar")]
 pub use grammar::JunoParser;
-pub use parser::parse_program;
-pub use metair::*;
-pub use grammar::JunoParserRule as Rule;
+#[cfg(feature = "grammar")]
 pub use pest;
-pub use phf;
-pub use builtin_registry::*;
-pub use ir::LLVMBackend;
+#[cfg(feature = "grammar")]
+pub use grammar::JunoParserRule as Rule;
+
+
+#[cfg(feature = "ast")]
+mod parser;
+#[cfg(feature = "ast")]
+pub use parser::parse_program;
+
+#[cfg(feature = "ast")]
+pub mod ast;
+
+
+#[cfg(feature = "metair")]
+mod metair;
+#[cfg(feature = "grammar")]
+pub use metair::*;
+
+
+#[cfg(feature = "irgen")]
+mod ir;
+#[cfg(feature = "irgen")]
 pub use inkwell;
+#[cfg(feature = "irgen")]
+pub use ir::LLVMBackend;
+
+
+#[cfg(feature = "compiler")]
+mod compile;
+#[cfg(feature = "compiler")]
 pub use compile::*;
+
+
+mod builtin_registry;
+pub use builtin_registry::*;
+pub use phf;
