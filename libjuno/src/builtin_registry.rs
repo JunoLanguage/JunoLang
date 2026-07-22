d40dae374ba6a89194ca3bdbf008e6aec3eb1eb9
TrollMii <trollmii@proton.me>
2026-07-22 14:50:49 +0200

    MPL Header

--- libjuno/src/builtin_registry.rs
+++ libjuno/src/builtin_registry.rs
@@ -1,3 +1,7 @@
+//This Source Code Form is subject to the terms of the Mozilla Public
+//License, v. 2.0. If a copy of the MPL was not distributed with this
+//file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use crate::SymbolId;
 use crate::*;
 use phf_macros::phf_map;
--- libjuno/tests/wrapper/testing_files.rs
+++ libjuno/tests/wrapper/testing_files.rs
@@ -1,3 +1,7 @@
+//This Source Code Form is subject to the terms of the Mozilla Public
+//License, v. 2.0. If a copy of the MPL was not distributed with this
+//file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use crate::wrapper::test_file_utils::test_file;
 
 const TEST0_JUNO: &str = include_str!("test_files/test0.juno");
